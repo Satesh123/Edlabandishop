b8c0462582f892a3c2978bca4ed4985aa5a62a6c
Satesh <janki.chaudhari@isoftbet.com>
2020-11-01 16:40:47 +0000

    update

--- src/test/java/regression/Test_Runner.java
+++ src/test/java/regression/Test_Runner.java
@@ -1,5 +1,6 @@
 package regression;
 
+import com.cucumber.listener.Reporter;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.AfterClass;
@@ -7,12 +8,14 @@ import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 
+import java.io.File;
 import java.net.MalformedURLException;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
 
-        plugin = {"pretty", "html:target/cucumber-html-report", "json:target/cucumber-json-report.json"}
+        plugin = {"pretty", "com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.html",
+                "json:target/cucumber-json-report.json"}
 
     //    ,tags = {"@02"}
 
@@ -25,10 +28,12 @@ public class Test_Runner {
     @BeforeClass
     public static void startBrowser() throws MalformedURLException {
         driver = BrowserFactory.getDriver();
+
     }
 
     @AfterClass
-    public static void stop() {
+    public static void writeExtentReport() {
+        Reporter.loadXMLConfig(new File("config/report.xml"));
         driver.quit();
         driver = null;
     }
